8963d15838ae2fe76f50ecc27c283abcb15d6f1c
thisisdj <dj8793@gmail.com>
2024-06-19 13:29:40 +0900

    fix: console 공개제한

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     reactStrictMode: false,
+    removeConsole: {
+        exclude: ["error", "warn"]
+    }
 };
 
 export default nextConfig;
